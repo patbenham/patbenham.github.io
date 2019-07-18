9d13b6e599aa99c846884f51fa967a25184537a2
Pat Benham <pabenham@gmail.com>
2019-07-18 10:13:06 -0500

    Created a sticky navigation menu. Created an about section and a contact form. Changed colors & styling.

--- stickynav.js
+++ stickynav.js
@@ -1,7 +1,8 @@
-var navbar = document.getElementById("stickybar");
+var navbar = document.getElementById("navbar");
 var navOffSetTop = navbar.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// Fix navbar to the top of viewport when reaching its scroll position
+// Unfix it when scrolling above its original position
 function stickyNav() {
   if (window.pageYOffset > navOffSetTop) {
     navbar.classList.add("navbar--fixed");
